c754c2f55a887cf395dec2bea58c46bcd5061e5f
Mateusz <mateusz.splawski96@gmail.com>
2020-11-27 21:27:14 +0100

    styling

--- src/components/Clock/Clock.styled.js
+++ src/components/Clock/Clock.styled.js
@@ -4,8 +4,11 @@ export const StyledClock = styled.div`
   position: relative;
   height: 65vh;
   width: 80vw;
-  background: ${({ theme }) => theme.colors.white};
-  border-radius: 20px;
+  background: ${({ theme }) =>
+    `linear-gradient(to right, ${theme.colors.white} 0%, ${theme.colors.white} 50%, ${theme.colors.black} 50%, ${theme.colors.black} 100%)`};
+  box-shadow: 0 0 20px -5px ${({ theme }) => theme.colors.shadow};
+  color: ${({ theme }) => theme.colors.darkWhite};
+  border-radius: 35px;
   padding: 30px;
   border: 2px solid black;
   overflow: hidden;
