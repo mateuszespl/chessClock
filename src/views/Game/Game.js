b53250395634357745c6ec9000af232f766daeef
Mateusz <mateusz.splawski96@gmail.com>
2020-12-02 19:09:02 +0100

    basic router setup

---
+++ src/views/Game/Game.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Clock from "../../components/Clock";
+
+import MainLayout from "../../layout/MainLayout";
+
+export const Game = () => {
+  return (
+    <MainLayout>
+      <Clock />
+    </MainLayout>
+  );
+};
---
+++ src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Form from "../../components/Form";
+import MainLayout from "../../layout/MainLayout";
+
+export const Home = () => {
+  return (
+    <MainLayout>
+      <Form />
+    </MainLayout>
+  );
+};
